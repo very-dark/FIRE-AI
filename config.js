be2a5641d61bd98934a3c062ad3d81b10bb277d2
very-dark <verydark468@gmail.com>
2024-11-18 10:04:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "2349013432338; Opaksfire"
+const ownervb = process.env.OWNERS || "2348057262878; Opaksfire"
 
 
 const ownerlist = ownervb.split(';');
@@ -23,8 +23,8 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2349013432338', '2349013432338']
-global.prems = ['2349013432338', '2349013432338', '2349013432338']
+global.mods = ['2348057262878', '2349013432338']
+global.prems = ['2348057262878', '2349013432338', '2349013432338']
 global.allowed = ['2349013432338', '2349013432338', '2349013432338']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
